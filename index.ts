2c6b0bd7f8c248007d98d4cd584af02975b331c3
suhyeonP <ahah12k@gmail.com>
2022-07-08 11:34:08 +0900

    :bug:

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-export * from './dist';
+export * from './dist/types';
+export * from './src/types';
+export * from './src/compareDate';
+export * from './src/resetTime';
+export * from './src/presetDate';
+export * from './src/getDateGap';
